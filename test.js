4c7d2105dd5014c211bcf695f71c8cbab1dca0c5
Abby Smith <smith@Abbys-MacBook-Pro.local>
2019-05-10 08:33:10 -0600

    first commit

---
+++ test.js
@@ -0,0 +1,13 @@
+// -- SELECT * FROM artist;
+// -- SELECT first_name, last_name, country FROM employee; 
+// -- SELECT name, composer, milliseconds
+// -- FROM track
+// -- WHERE milliseconds > 299000; 
+// -- SELECT count(*)
+// -- FROM track 
+// -- WHERE milliseconds > 299000; 
+// -- SELECT AVG(milliseconds) 
+// -- FROM track 
+// -- SELECT * FROM invoice 
+// -- SELECT COUNT(*) FROM invoice 
+// -- WHERE billing_country = 'USA'; 
