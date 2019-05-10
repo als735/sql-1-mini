6d708b2311597e87b149233844ed2072591fff21
Abby Smith <smith@Abbys-MacBook-Pro.local>
2019-05-10 08:33:56 -0600

    second commit

--- test.js
+++ test.js
@@ -11,3 +11,13 @@
 // -- SELECT * FROM invoice 
 // -- SELECT COUNT(*) FROM invoice 
 // -- WHERE billing_country = 'USA'; 
+// -- SELECT first_name FROM customer WHERE first_name LIKE 'A%'; 
+// -- SELECT * FROM track ORDER BY milliseconds ASC LIMIT 10; 
+// -- SELECT * FROM track ORDER BY milliseconds DESC LIMIT 20 ; 
+// -- SELECT * FROM customer WHERE state = 'California' OR state = 'Washington'; 
+// -- SELECT * FROM customer WHERE state IN ('California', 'Washington', 'Utah', 'Florida', 'Arizona');
+// -- SELECT * FROM playlist; 
+// -- SELECT * FROM playlist_track; 
+// Insert an artist to the database
+// Insert yourself as a customer to the database
+// Find a list of all Playlists that start with Classical
